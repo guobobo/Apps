585894beb250078eaf38b9b9fe597225b61aadd3
guobobo <1711453638@qq.com>
2017-07-10 17:10:33 +0800

    壮壮1

--- app/src/main/java/com/example/iu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/iu/myapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.iu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//sb
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
