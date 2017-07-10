48247b66459d14c479990dc92fdf56b6735db06b
guobobo <1711453638@qq.com>
2017-07-10 17:18:01 +0800

    壮壮2

--- app/src/main/java/com/example/iu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/iu/myapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.iu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//我是壮壮去去去
+//啦啦啦
 public class MainActivity extends AppCompatActivity {
 
     @Override
