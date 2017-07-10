3fa8f3f90072402d36f7a535d8bcf34659fe0aca
gulajiasiqi <1211039589@qq.com>
2017-07-10 23:54:32 +0800

    赵辉的友盟第三方的集成

---
+++ app/src/main/java/com/example/iu/myapplication/App.java
@@ -0,0 +1,29 @@
+package com.example.iu.myapplication;
+
+import android.app.Application;
+
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
+
+/**
+ * Created by dell on 2017/5/24.
+ */
+
+public class App extends Application {
+
+    {
+        PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
+        PlatformConfig.setQQZone("1106204691", "KEguRaDKDSPfp7tH");
+        PlatformConfig.setSinaWeibo("1770387667", "96551cfcd196b8bfa51d316b49a1bc42", "http://sns.whalecloud.com");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        UMShareAPI.get(this);
+
+        MobclickAgent.openActivityDurationTrack(false);
+    }
+}
--- app/src/main/java/com/example/iu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/iu/myapplication/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.iu.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+
+
 //啦啦啦
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.mian_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                UMImage image = new UMImage(MainActivity.this,"http://img6.faloo.com/Picture/0x0/0/160/160001.jpg");
+
+                UMImage  umImage = new UMImage(MainActivity.this, R.mipmap.ic_launcher);
+
+                image.setThumb(umImage);
+
+                new ShareAction(MainActivity.this).withText("hello").withMedia(image)
+                        .setDisplayList(SHARE_MEDIA.SINA,SHARE_MEDIA.QQ,SHARE_MEDIA.WEIXIN,SHARE_MEDIA.QZONE)
+                        .setCallback(new UMShareListener() {
+                            @Override
+                            public void onStart(SHARE_MEDIA share_media) {
+
+                            }
+
+                            @Override
+                            public void onResult(SHARE_MEDIA share_media) {
+
+                            }
+
+                            @Override
+                            public void onError(SHARE_MEDIA share_media, Throwable throwable) {
+
+                            }
+
+                            @Override
+                            public void onCancel(SHARE_MEDIA share_media) {
+
+                            }
+                        }).open();
+
+            }
+        });
     }
 }
