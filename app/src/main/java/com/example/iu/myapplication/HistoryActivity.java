ccca453cc3e4b353f830df1bb7595584fbf5d1cd
huanchenchen <1415737202@qq.com>
2017-07-24 16:11:40 +0800

    cx

--- app/src/main/java/com/example/iu/myapplication/HistoryActivity.java
+++ app/src/main/java/com/example/iu/myapplication/HistoryActivity.java
@@ -1,11 +1,11 @@
 package com.example.iu.myapplication;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.iu.myapplication.base.BaseActivity;
 import com.example.iu.myapplication.model.dao.DaoMaster;
@@ -30,21 +30,23 @@ public class HistoryActivity extends BaseActivity {
     @Bind(R.id.history_backImage)
     ImageView historyBackImage;
     @Bind(R.id.history_editBtn)
-    Button historyEditBtn;
+    TextView historyEditBtn;
     @Bind(R.id.history_listview)
-    ListView historyListview;
+    RecyclerView historyListview;
     @Bind(R.id.history_item_chooseBtn)
-    Button historyItemChooseBtn;
+    TextView historyItemChooseBtn;
     @Bind(R.id.history_item_deleteBtn)
-    Button historyItemDeleteBtn;
+    TextView historyItemDeleteBtn;
     @Bind(R.id.liner_chooseAnddelete)
     LinearLayout linerChooseAnddelete;
 
     private LinkedList<History> mList = new LinkedList<History>();
-    private ArrayList<HistoryBean>  hList=new ArrayList<>();
-    private HistoryAdapter adapter;
+    private ArrayList<HistoryBean> hList = new ArrayList<>();
+//    private HistoryAdapter adapter;
     private boolean flag = false;
     private int number = 0;
+
+    private HistoryAdapterRecy adapterRecy;
 
     @Override
     public int getLayoutId() {
@@ -54,83 +56,105 @@ public class HistoryActivity extends BaseActivity {
     @Override
     public void initView() {
         select();
-            historyEditBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String btnText = historyEditBtn.getText().toString();
-                    if(btnText.equals("编辑")){
-                        for(int i = 0; i < hList.size(); i++) {
-                            hList.get(i).setVisibility(true);
-                        }
-                        linerChooseAnddelete.setVisibility(View.VISIBLE);
-                        historyEditBtn.setText("取消");
-                        adapter.notifyDataSetChanged();
-
-                    }else if(btnText.equals("取消")){
-                        for(int i = 0; i < hList.size(); i++) {
-                            hList.get(i).setVisibility(false);
-                        }
-                        linerChooseAnddelete.setVisibility(View.GONE);
-                        historyEditBtn.setText("编辑");
-                        adapter.notifyDataSetChanged();
-                    }
-                }
-            });
-
-        historyItemChooseBtn.setOnClickListener(new View.OnClickListener() {
+        adapterRecy.setOnHistory(new HistoryAdapterRecy.OnHistory() {
             @Override
-            public void onClick(View v) {
-
-                String historyBtn = historyItemChooseBtn.getText().toString();
-
-                if(historyBtn.equals("全选")){
-                    for(int i = 0; i < hList.size(); i++) {
-                        hList.get(i).setFlag(true);
+            public void onHistoryListener(int position) {
+                if(historyEditBtn.getText().toString().equals("取消")){
+                    if (hList.get(position).isFlag() == false) {
+                        hList.get(position).setFlag(true);
+                        number++;
+                        historyItemDeleteBtn.setText("删除"+number);
+                    } else {
+                        hList.get(position).setFlag(false);
+                        number--;
+                        historyItemDeleteBtn.setText("删除"+number);
                     }
-                    historyItemChooseBtn.setText("取消");
-                    historyItemDeleteBtn.setText("删除"+"("+hList.size()+")");
-                    historyItemDeleteBtn.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            historyEditBtn.setText("编辑");
-                            linerChooseAnddelete.setVisibility(View.GONE);
-                            historyItemDeleteBtn.setText("删除");
-                        }
-                    });
-                }else if(historyBtn.equals("取消")){
-                    for(int i = 0; i < hList.size(); i++) {
-                        hList.get(i).setFlag(false);
+                    if (number == 0) {
+                        historyItemDeleteBtn.setText("删除");
                     }
-                    historyItemChooseBtn.setText("全选");
-                    historyItemDeleteBtn.setText("删除");
+                    if(number==hList.size()){
+                        historyItemChooseBtn.setText("取消全选");
+                    }else {
+                        historyItemChooseBtn.setText("全选");
+                    }
 
                 }
+                adapterRecy.notifyDataSetChanged();
             }
         });
 
-        historyListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                HistoryBean historyBean = hList.get(position);
-                if(historyBean.isFlag()==false){
-                    historyBean.setFlag(true);
-                    number++;
-                    historyItemDeleteBtn.setText("删除"+"("+number+")");
-                }else {
-                    number--;
-                    historyItemDeleteBtn.setText("删除"+"("+number+")");
-                    historyBean.setFlag(false);
-                }
-                adapter.notifyDataSetChanged();
-            }
-        });
     }
 
-    @OnClick({R.id.history_backImage})
+    @OnClick({R.id.history_backImage, R.id.history_editBtn, R.id.history_item_chooseBtn, R.id.history_item_deleteBtn})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.history_backImage:
                 finish();
+                break;
+            case R.id.history_editBtn:
+
+                if (historyEditBtn.getText().equals("编辑")) {
+                    for (int i = 0; i < hList.size(); i++) {
+                        hList.get(i).setVisibility(true);
+                    }
+                    linerChooseAnddelete.setVisibility(View.VISIBLE);
+                    historyEditBtn.setText("取消");
+                    adapterRecy.notifyDataSetChanged();
+                } else {
+                    for (int i = 0; i < hList.size(); i++) {
+                        hList.get(i).setVisibility(false);
+                    }
+                    for (int i = 0; i < hList.size(); i++) {
+                        hList.get(i).setFlag(false);
+                    }
+                    linerChooseAnddelete.setVisibility(View.GONE);
+                    historyEditBtn.setText("编辑");
+                    historyItemChooseBtn.setText("全选");
+                    historyItemDeleteBtn.setText("删除");
+                    number=0;
+                    adapterRecy.notifyDataSetChanged();
+                }
+
+                break;
+            case R.id.history_item_chooseBtn:
+                if (historyItemChooseBtn.getText().equals("全选")) {
+                    historyItemChooseBtn.setText("取消全选");
+                    if (historyEditBtn.getText().equals("取消")) {
+                        for (int i = 0; i < hList.size(); i++) {
+                            hList.get(i).setFlag(true);
+                        }
+                        number = hList.size();
+                        historyItemDeleteBtn.setText("删除" + number);
+                        adapterRecy.notifyDataSetChanged();
+                    }
+                } else {
+                    for (int i = 0; i < hList.size(); i++) {
+                        hList.get(i).setFlag(false);
+                    }
+                    number = 0;
+                    historyItemDeleteBtn.setText("删除");
+                    historyItemChooseBtn.setText("全选");
+                    adapterRecy.notifyDataSetChanged();
+                }
+
+                break;
+            case R.id.history_item_deleteBtn:
+                if (historyEditBtn.getText().equals("取消")) {
+                    for (int i = hList.size() - 1; i >= 0; i--) {
+                        if (hList.get(i).isFlag()) {
+                            deleteAll();
+                            hList.remove(hList.get(i));
+                        }
+                    }
+                    number = 0;
+                    adapterRecy.notifyDataSetChanged();
+                    historyItemDeleteBtn.setText("删除");
+                    if (hList.size() == 0) {
+                        historyEditBtn.setVisibility(View.GONE);
+                        linerChooseAnddelete.setVisibility(View.GONE);
+                    }
+                }
+
                 break;
         }
     }
@@ -147,19 +171,23 @@ public class HistoryActivity extends BaseActivity {
         QueryBuilder<History> builder = historyDao.queryBuilder();
 
         List<History> list = builder.list();
+
         hList.clear();
-        for(int i = 0; i < list.size(); i++) {
-            hList.add(new HistoryBean(list.get(i).getTitle(),list.get(i).getImageUrl(),list.get(i).getVoideLength(),list.get(i).getDayTime(),false,false));
+
+        for (int i = 0; i < list.size(); i++) {
+            hList.add(new HistoryBean(list.get(i).getTitle(), list.get(i).getImageUrl(), list.get(i).getVoideLength(), list.get(i).getDayTime(), false, false));
         }
 
         mList.clear();
-        adapter = new HistoryAdapter(hList, this,flag);
-        historyListview.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
+//        adapter = new HistoryAdapter(hList, this, flag);
+//        historyListview.setAdapter(adapter);
+//        adapter.notifyDataSetChanged();
+        adapterRecy=new HistoryAdapterRecy(this,hList);
+        historyListview.setLayoutManager(new LinearLayoutManager(this));
+        historyListview.setAdapter(adapterRecy);
     }
 
     public void deleteAll() {
-
 
         DaoMaster master = new DaoMaster(MyHistoryHelper.gethelper(HistoryActivity.this).getW());
 
@@ -171,7 +199,7 @@ public class HistoryActivity extends BaseActivity {
     }
 
 
-    public void updata(History history){
+    public void updata(History history) {
 
         DaoMaster master = new DaoMaster(MyHistoryHelper.gethelper(this).getW());
 
